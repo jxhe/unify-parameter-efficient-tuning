82672b345e643b20920886c86a1a5024813eb251
Patrick von Platen <patrick.v.platen@gmail.com>
2020-04-06 13:32:46 +0200

    update init

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -186,6 +186,7 @@ if is_torch_available():
         BertForQuestionAnswering,
         load_tf_weights_in_bert,
         BERT_PRETRAINED_MODEL_ARCHIVE_MAP,
+        BertLayer
     )
     from .modeling_openai import (
         OpenAIGPTPreTrainedModel,
@@ -311,6 +312,7 @@ if is_torch_available():
         ElectraModel,
         load_tf_weights_in_electra,
         ELECTRA_PRETRAINED_MODEL_ARCHIVE_MAP,
+    )
 
     from .modeling_reformer import (
         ReformerAttention,
