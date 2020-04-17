dff6356cf87ee38cb668a4e18f5e35dd6999696c
Patrick von Platen <patrick.v.platen@gmail.com>
2020-04-17 11:23:38 +0200

    make style

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -187,7 +187,7 @@ if is_torch_available():
         BertForQuestionAnswering,
         load_tf_weights_in_bert,
         BERT_PRETRAINED_MODEL_ARCHIVE_MAP,
-        BertLayer
+        BertLayer,
     )
     from .modeling_openai import (
         OpenAIGPTPreTrainedModel,
@@ -315,12 +315,7 @@ if is_torch_available():
         ELECTRA_PRETRAINED_MODEL_ARCHIVE_MAP,
     )
 
-    from .modeling_reformer import (
-        ReformerAttention,
-        ReformerLayer,
-        ReformerModel,
-        ReformerModelWithLMHead
-    )
+    from .modeling_reformer import ReformerAttention, ReformerLayer, ReformerModel, ReformerModelWithLMHead
 
     # Optimization
     from .optimization import (
