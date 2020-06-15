b5099747f93f63b0ee07731567210df998612dbc
sshleifer <sshleifer@gmail.com>
2020-06-15 17:36:56 -0400

    boom boom

--- examples/summarization/test_summarization_examples.py
+++ examples/summarization/test_summarization_examples.py
@@ -187,7 +187,7 @@ class TestSummarizationDistiller(unittest.TestCase):
         evaluate_checkpoint(ckpts[0], dest_dir=Path(tempfile.mkdtemp()))
 
     def test_bdc_theseus(self):
-        updates = dict(theseus_replace_rate=0.5, student_encoder_layers=1, student_decoder_layers=1, no_teacher=True)
+        updates = dict(theseus_replace_rate=0.5, student_encoder_layers=1, student_decoder_layers=1, no_teacher=True, theseus_init_copy=True)
         self._bart_distiller_cli(updates)
 
     def test_bdc_frozen_theseus(self):
