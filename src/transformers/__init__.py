6c8340234fd6525e568e7d5b60a48678a5bc9e75
Patrick von Platen <patrick.v.platen@gmail.com>
2020-08-06 15:39:00 +0000

    fix some tests

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -570,6 +570,7 @@ if is_tf_available():
         TFLongformerModel,
         TFLongformerForMaskedLM,
         TFLongformerForQuestionAnswering,
+        TFLongformerSelfAttention,
     )
 
     from .modeling_tf_mobilebert import (
