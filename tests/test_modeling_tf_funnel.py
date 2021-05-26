c5d3b738e65544a1456a465df907c649c51b780c
Lysandre <lysandre.debut@reseau.eseo.fr>
2021-05-26 11:42:26 +0200

    Test

--- tests/test_modeling_tf_funnel.py
+++ tests/test_modeling_tf_funnel.py
@@ -17,7 +17,7 @@
 import unittest
 
 from transformers import FunnelConfig, is_tf_available
-from transformers.testing_utils import require_tf
+from transformers.testing_utils import require_tf, slow
 
 from .test_configuration_common import ConfigTester
 from .test_modeling_tf_common import TFModelTesterMixin, ids_tensor
@@ -325,6 +325,7 @@ class TFFunnelModelTester:
         return config, inputs_dict
 
 
+@slow
 @require_tf
 class TFFunnelModelTest(TFModelTesterMixin, unittest.TestCase):
     all_model_classes = (
