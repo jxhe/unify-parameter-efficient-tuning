814e441855416dacb3e67de24eb59eadbdb543a6
violet-zct <chuntinz@cs.cmu.edu>
2021-09-26 02:01:29 -0400

    minor

--- exps/run_bart_trainer_lora_xm.sh
+++ exps/run_bart_trainer_lora_xm.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #SBATCH --output=slurm_logs/slurm-%A-%a.out
 #SBATCH --error=slurm_logs/slurm-%A-%a.err
-#SBATCH --job-name=xsum.lora.attn.30.4
+#SBATCH --job-name=xsum.lora.attn.1.4
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:v100:1
 #SBATCH --partition=gpu
@@ -59,13 +59,13 @@ ffn_bn_len=1
 lora_alpha=4
 
 # xsum.lora.attn.30.4
-attn_mode="lora"
-attn_option="none"
-ffn_mode="none"
-ffn_option="none"
-preseqlen=30
-ffn_bn_len=1
-lora_alpha=120
+#attn_mode="lora"
+#attn_option="none"
+#ffn_mode="none"
+#ffn_option="none"
+#preseqlen=30
+#ffn_bn_len=1
+#lora_alpha=120
 
 # xsum.lora.attn.400.4
 #attn_mode="lora"
